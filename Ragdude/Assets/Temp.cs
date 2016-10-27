44e27c03d42ea1442b84189f96ace39f69848139
warddav16 <warddav16@gmail.com>
2016-10-26 19:12:04 -0500

    initial commit

---
+++ Ragdude/Assets/Temp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Temp : MonoBehaviour {
+    public GameObject temp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
