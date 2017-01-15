3cb8dbc97f8db237fbf4edd3eb6e71cf82f33258
warddav16 <warddav16@gmail.com>
2017-01-15 13:01:01 -0600

    enemy ragdolls on click

---
+++ RagdollTest/Assets/Scripts/Enemy.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    enum State
+    {
+        ComingForYa, 
+        Ragdoll,
+        StandinUp
+    }
+
+    Animator _animator;
+    Rigidbody[] _bodies;
+    State _currState = State.ComingForYa;
+    Player _player;
+    NavMeshAgent _navAgent;
+    Collider _collider;
+    float _downTimeTimer = 0.0f;
+
+
+    public float DownTime = 3.0f;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _bodies = GetComponentsInChildren<Rigidbody>();
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        _navAgent = GetComponent<NavMeshAgent>();
+        _collider = GetComponent<Collider>();
+
+        ToggleRagdoll(false);
+    }
+
+    void ToggleRagdoll( bool toggle )
+    {
+        _animator.enabled = !toggle;
+
+        foreach ( Rigidbody rb in _bodies )
+        {
+            rb.isKinematic = !toggle;
+        }
+    }
+
+    public void TakeHit()
+    {
+        if ( _currState == State.ComingForYa )
+        {
+            _currState = State.Ragdoll;
+            ToggleRagdoll( true );
+            _downTimeTimer = 0.0f;
+            _navAgent.enabled = false;
+        }
+    }
+
+    public void OnIsStanding()
+    {
+        _currState = State.ComingForYa;
+        _navAgent.enabled = true;
+    }
+
+    void Update()
+    {
+        switch( _currState )
+        {
+            case State.ComingForYa:
+                _navAgent.SetDestination( _player.transform.position );
+                break;
+            case State.Ragdoll:
+                _downTimeTimer += Time.deltaTime;
+                if( _downTimeTimer >= DownTime )
+                {
+                    ToggleRagdoll( false );
+                    _currState = State.StandinUp;
+                }
+                break;
+            case State.StandinUp:
+                break;
+        }
+    }
+}
--- RagdollTest/Assets/Scripts/Player.cs
+++ RagdollTest/Assets/Scripts/Player.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    Camera _cam;
+    public float Knockback = 1.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Awake()
+    {
+        _cam = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        if( Input.GetMouseButtonDown(0) )
+        {
+            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit outInfo;
+            if( Physics.Raycast(ray, out outInfo) )
+            {
+                Enemy enemy = outInfo.collider.transform.root.GetComponent<Enemy>();
+                if( enemy )
+                {
+                    enemy.TakeHit();
+                    Vector3 knockbackDir = ( outInfo.point - _cam.transform.position ).normalized;
+                    outInfo.rigidbody.AddForce(knockbackDir * Knockback, ForceMode.Impulse);
+                }
+            }
+        }
+    }
 }
